3585b82dba472cda0ba8f2e424e52d197d19a21f
KianDoyle <kianrk@icloud.com>
2025-04-16 16:49:22 +0100

    Added beginnings of charting

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -11,10 +11,31 @@ const Sidebar = () => {
             <img src="/navigation/search-icon.png" alt="Search Icon" className="widget-img" />
             <span className="widget-title">SEARCH</span>
         </Link>
+        <Link to="/" className="widget-icon">
+            <img src="/navigation/showdown-icon.png" alt="Showdown Icon" className="widget-img" />
+            <span className="widget-title">SHOWDOWN</span>
+        </Link>
+        <Link to="/" className="widget-icon">
+            <img src="/navigation/continents-icon.png" alt="Continents Icon" className="widget-img" />
+            <span className="widget-title">CONTINENTS</span>
+        </Link>
+        <Link to="/" className="widget-icon">
+            <img src="/navigation/search-icon.png" alt="Tracker Icon" className="widget-img" />
+            <span className="widget-title">TRACKER</span>
+        </Link>
+        <Link to="/" className="widget-icon">
+            <img src="/navigation/search-icon.png" alt="Visualise Icon" className="widget-img" />
+            <span className="widget-title">VISUALISE</span>
+        </Link>
+        <Link to="/" className="widget-icon">
+            <img src="/navigation/search-icon.png" alt="Predict Icon" className="widget-img" />
+            <span className="widget-title">PREDICT</span>
+        </Link>
         <div className="widget-icon-spacer"></div>
-        <div className="widget-icon">
-          ACCOUNT
-        </div>
+        <Link to="/" className="widget-icon">
+            <img src="/navigation/account-icon.png" alt="Account Icon" className="widget-img" />
+            <span className="widget-title">ACCOUNT</span>
+        </Link>
       </nav>
     </aside>
   );
